c018102d137451c50601d9a6215e8d23a7410b3a
Mark Boleigha <prof_evil@icloud.com>
2023-06-12 20:24:53 +0100

    add permissions and roles model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Spatie\Permission\Contracts\HasPermissions;
 
-class User extends Authenticatable
+
+class User extends Authenticatable implements HasPermissions
 {
     use HasApiTokens, HasFactory, Notifiable;
 
