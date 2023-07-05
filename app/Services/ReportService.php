a8c2191bacfe71e15f90c08c5bd4cede0c53f558
Mark Boleigha <prof_evil@icloud.com>
2023-07-05 17:40:51 +0100

    fix class errors on reports

--- app/Services/ReportService.php
+++ app/Services/ReportService.php
@@ -20,14 +20,14 @@ use App\Models\Expense;
 
 class ReportService {
     
-    public function getSupplierPaymentByWarehouse(int $id) {
+    public static function getSupplierPaymentByWarehouse(int $id) {
         $payments = SupplierPayment::whereHas('transaction', function ($query) use ($id) {
             $query->where('warehouse_id', $id);
         })->paginate(25);
         return $payments;
     }
 
-    public function getAllSupplierPayment() {
+    public static function getAllSupplierPayment() {
         return SupplierPayment::orderBy('created_at', 'desc')->paginate(25);
     }
 
