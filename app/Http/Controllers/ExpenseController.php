2cb2388b5d36e1a088e69055edc8348e9e5a38d9
Mark Boleigha <prof_evil@icloud.com>
2023-08-22 12:32:28 +0100

    minor fix

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -29,14 +29,21 @@ class ExpenseController extends Controller
         if($user->hasRole('admin')) {
             $expenses = Expense::orderBy('created_at','desc')->paginate(25);
         } else if($user->hasRole('manager')) {
-            $warehouseId = $user->warehouse->id;
-            // dd($warehouseId);
-            $expenses = Expense::whereHas('staff', function ($query) use ($warehouseId) {
-                $query->whereHas('warehouse', function ($subQuery) use ($warehouseId) {
-                    $subQuery->where('warehouse_id', $warehouseId);
-                });
-            })->paginate(25);
-            // dd($expenses);
+
+            $warehouseId = $user->warehouse()->id;
+            if($warehouseId) {
+                $expenses = Expense::whereHas('staff', function ($query) use ($warehouseId) {
+                    $query->whereHas('warehouse', function ($subQuery) use ($warehouseId) {
+                        $subQuery->where('warehouse_id', $warehouseId);
+                    });
+                })->paginate(25);
+            } else {
+                $collection = new Collection();
+                // $currentPageItems = $collection->slice($offset, $perPage)->all();
+                $expenses= new LengthAwarePaginator($collection, count($collection), 0, 1);
+                // Set the path for the paginator
+                $expenses->setPath(Request::url());
+            }
         } else {
 
         }
